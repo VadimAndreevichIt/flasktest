a9b9beb2c3ad1e1e421157ed03f89f3a35b654b8
vadyu <yramishin0964@gmail.com>
2022-12-29 12:13:09 +0300

    Version 1.0

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return '''
+    <htnl>
+        Привет!!!
+    </html>
+    '''
+
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
